93229ae98540e5dcb00e2241963b854b810c1dbe
Anabelagarcia <anabelanams@gmail.com>
2024-10-31 14:57:43 -0400

    bug2

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('bug1').addEventListener('click', function() {
+    console.log("Bug 1: Ação não definida"); 
+});
+
+document.getElementById('bug2').addEventListener('click', function() {
+    
+    alert(document.getElementById('inputInexistente').value);
+});
+
+
